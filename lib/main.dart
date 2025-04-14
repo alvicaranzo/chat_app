a5c3a8a7ccb5919c16bf4134f484ec4b457742b8
alvic caranzo <=>
2025-04-14 09:14:18 +0800

    Getting inputs from the user

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class ChatApp extends StatelessWidget {
     return MaterialApp(
       title: "Chat App",
       theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: const LoginPage(),
+      home: LoginPage(),
     );
   }
 }
