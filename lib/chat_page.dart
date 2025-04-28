c13e26dac7a1fefde327b6f393b048f666036e02
alvic caranzo <=>
2025-04-28 19:02:48 +0800

    Generics in Dart

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -5,7 +5,27 @@ import 'package:flutter/material.dart';
 import 'models/chat_message_entity.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  ChatPage({Key? key}) : super(key: key);
+
+  List<ChatMessageEntity> _messages = [
+    ChatMessageEntity(
+        author: Author(userName: 'pooja'),
+        createdAt: 2131231242,
+        id: '1',
+        text: 'First text'),
+    ChatMessageEntity(
+        author: Author(userName: 'pooja'),
+        createdAt: 2131231442,
+        id: '1',
+        text: 'Second text',
+        imageUrl: 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png'),
+    ChatMessageEntity(
+      author: Author(userName: 'jane'),
+      createdAt: 2131234242,
+      id: '1',
+      text: 'Third text',
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,17 +49,13 @@ class ChatPage extends StatelessWidget {
         children: [
           Expanded(
               child: ListView.builder(
-                  itemCount: 10,
+                  itemCount: _messages.length,
                   itemBuilder: (context, index) {
                     return ChatBubble(
-                        alignment: index % 2 == 0
-                            ? Alignment.centerLeft
-                            : Alignment.centerRight,
-                        entity: ChatMessageEntity(
-                            id: '1234',
-                            text: 'Hello this is Alvic!!!!!',
-                            createdAt: DateTime.now().microsecondsSinceEpoch,
-                            author: Author(userName: 'alvicaranzo')));
+                        alignment: _messages[index].author.userName == 'pooja'
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        entity: _messages[index]);
                   })),
           ChatInput(),
         ],
