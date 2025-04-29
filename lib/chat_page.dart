cc4cf4dbd6db1caddf283e64855251bf3dae6c17
alvic caranzo <=>
2025-04-29 21:52:47 +0800

    FutureBuilder for futures

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -46,7 +46,7 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   //TODO: Get Network Images from API
-  _getNetworkImages() async {
+  Future<List<PixelfordImage>> _getNetworkImages() async {
     var endpointUrl = Uri.parse('https://picsum.photos/v2/list');
 
     final response = await http.get(endpointUrl);
@@ -58,6 +58,9 @@ class _ChatPageState extends State<ChatPage> {
         return PixelfordImage.fromJson(listItem);
       }).toList();
       print(_imageList[0].urlFullSize);
+      return _imageList;
+    } else {
+      throw Exception('API not successful!');
     }
   }
 
@@ -90,6 +93,14 @@ class _ChatPageState extends State<ChatPage> {
       ),
       body: Column(
         children: [
+          FutureBuilder<List<PixelfordImage>>(
+              future: _getNetworkImages(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<PixelfordImage>> snapshot) {
+                if (snapshot.hasData)
+                  return Image.network(snapshot.data![0].urlFullSize);
+                return CircularProgressIndicator();
+              }),
           Expanded(
               child: ListView.builder(
                   itemCount: _messages.length,
